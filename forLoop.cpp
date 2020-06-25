ce20ec4daf0eb30638ed65ebb2256a3792354a4f
Ankit Lenka <ankitlenka26@gmail.com>
2020-06-25 17:18:29 +0530

    Let's see

--- forLoop.cpp
+++ forLoop.cpp
@@ -12,3 +12,6 @@ int main(){
     // now every comment i write is going inside the dev branch
     // new change in dev branch  
 }
+
+// hey there i am ankit 
+// i hope now my account have changed to newzera
